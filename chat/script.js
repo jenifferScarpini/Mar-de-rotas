61fb2a1dcf0ce20c0294ffcfd2e2a655927f61d7
JenifferScarpini <128192490+jenifferScarpini@users.noreply.github.com>
2024-11-03 21:08:07 -0300

    criaçao menu

---
+++ chat/script.js
@@ -0,0 +1,13 @@
+// script.js
+function toggleMenu() {
+  const menuContent = document.querySelector('.menu-content');
+  menuContent.style.display = menuContent.style.display === 'block' ? 'none' : 'block';
+}
+
+function createSchedule() {
+  alert('Função para criar novo horário');
+}
+
+function viewSchedules() {
+  alert('Função para ver horários existentes');
+}
